7abae42d6a56b835a60be295205f9291297b29d0
Mo ElBoB <mo.elbob2005@gmail.com>
2026-03-11 17:14:17 +0200

    collect static files for swagger

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -98,6 +98,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 
 # إعدادات Django REST Framework
 REST_FRAMEWORK = {
